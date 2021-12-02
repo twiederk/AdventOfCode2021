eb955901f7ab662f1eeb819f1f769c4d536ec33f
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-12-02 19:12:18 +0100

    Implemented dive

--- Day2_Dive/src/main/java/com/d20charactersheet/aoc2001/day2/Dive.kt
+++ Day2_Dive/src/main/java/com/d20charactersheet/aoc2001/day2/Dive.kt
@@ -1,8 +1,18 @@
 package com.d20charactersheet.aoc2001.day2
 
 class Dive {
+
     fun dive(plannedCourse: List<NaviCommand>): Int {
-        return 0
+        var horizontal = 0
+        var depth = 0
+        for (naviCommand in plannedCourse) {
+            when (naviCommand.direction) {
+                Direction.FORWARD -> horizontal += naviCommand.value
+                Direction.DOWN -> depth += naviCommand.value
+                Direction.UP -> depth -= naviCommand.value
+            }
+        }
+        return horizontal * depth
     }
 
 }
