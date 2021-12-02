b5cce05c3621137a6e16f33a8e908ee9dd1c8165
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-12-02 04:56:37 +0100

    Implemented sum of sliding window

--- Day1_SonarSweep/src/main/java/com/d20charactersheet/aoc2021/day1/SonarSweep.kt
+++ Day1_SonarSweep/src/main/java/com/d20charactersheet/aoc2021/day1/SonarSweep.kt
@@ -26,16 +26,12 @@ class SonarSweep {
     }
 
     fun sumSlidingWindows(data: List<Int>): List<Int> {
-        return listOf(
-            data.slice(0..2).sum(),
-            data.slice(1..3).sum(),
-            data.slice(2..4).sum(),
-            data.slice(3..5).sum(),
-            data.slice(4..6).sum(),
-            data.slice(5..7).sum(),
-            data.slice(6..8).sum(),
-            data.slice(7..9).sum(),
-        )
+        val sums = mutableListOf<Int>()
+        for (index in 0..data.size - 3) {
+            val range = IntRange(index, index + 2)
+            sums.add(data.slice(range).sum())
+        }
+        return sums.toList()
     }
 
 }
