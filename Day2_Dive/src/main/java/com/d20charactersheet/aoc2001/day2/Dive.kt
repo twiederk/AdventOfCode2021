a63517852e9bf30c0e480ad8bb9f4981a7c64e03
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-12-02 19:19:05 +0100

    Load raw data and convert it to planned course

--- Day2_Dive/src/main/java/com/d20charactersheet/aoc2001/day2/Dive.kt
+++ Day2_Dive/src/main/java/com/d20charactersheet/aoc2001/day2/Dive.kt
@@ -1,5 +1,8 @@
 package com.d20charactersheet.aoc2001.day2
 
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
 class Dive {
 
     fun dive(plannedCourse: List<NaviCommand>): Int {
@@ -13,6 +16,18 @@ class Dive {
             }
         }
         return horizontal * depth
+    }
+
+    fun loadCourse(filename: String): List<NaviCommand> {
+        val rawCourse = Path(filename).readLines()
+        val plannedCourse = mutableListOf<NaviCommand>()
+        for (rawNaviCommand in rawCourse) {
+            val paramsNaviCommand = rawNaviCommand.split(' ')
+            val direction = Direction.valueOf(paramsNaviCommand[0].uppercase())
+            val value = paramsNaviCommand[1].toInt()
+            plannedCourse.add(NaviCommand(direction, value))
+        }
+        return plannedCourse
     }
 
 }
--- Day2_Dive/src/test/java/com/d20charactersheet/aoc2001/day2/DiveTest.kt
+++ Day2_Dive/src/test/java/com/d20charactersheet/aoc2001/day2/DiveTest.kt
@@ -5,15 +5,26 @@ import org.junit.jupiter.api.Test
 
 class DiveTest {
 
+
+    @Test
+    fun loadCourse() {
+
+        // act
+        val plannedCourse = Dive().loadCourse(".\\src\\test\\resources\\planned_course_test.txt")
+
+        // assert
+        assertThat(plannedCourse).containsExactly(
+            NaviCommand(Direction.FORWARD, 5),
+            NaviCommand(Direction.DOWN, 5),
+            NaviCommand(Direction.FORWARD, 8),
+            NaviCommand(Direction.UP, 3),
+            NaviCommand(Direction.DOWN, 8),
+            NaviCommand(Direction.FORWARD, 2),
+        )
+    }
+
     @Test
     fun dive() {
-        // arrange
-//        forward 5
-//        down 5
-//        forward 8
-//        up 3
-//        down 8
-//        forward 2
         val plannedCourse = listOf(
             NaviCommand(Direction.FORWARD, 5),
             NaviCommand(Direction.DOWN, 5),
