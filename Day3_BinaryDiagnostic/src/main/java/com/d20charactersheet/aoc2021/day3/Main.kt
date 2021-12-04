51c0abcde78859e665f3dd745bea04bd8a2503fc
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-12-04 05:05:22 +0100

    Added puzzle input and main function

---
+++ Day3_BinaryDiagnostic/src/main/java/com/d20charactersheet/aoc2021/day3/Main.kt
@@ -0,0 +1,12 @@
+package com.d20charactersheet.aoc2021.day3
+
+fun main(args: Array<String>) {
+
+    val binaryDiagnostic = BinaryDiagnostic()
+    val report = binaryDiagnostic.loadBinaryDiagnostic(args[0])
+    val gammaRate = binaryDiagnostic.readGammaRate(report)
+    val epsilonRate = binaryDiagnostic.createEpsilonRate(gammaRate)
+    val powerConsumption = gammaRate * epsilonRate
+
+    println("powerConsumption = $powerConsumption")
+}
