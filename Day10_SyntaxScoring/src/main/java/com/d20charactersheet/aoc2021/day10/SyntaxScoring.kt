c35882aa74a6b177420658d3d5a747bb8a42c269
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-12-12 19:29:03 +0100

    Implemented checkSyntax of line

--- Day10_SyntaxScoring/src/main/java/com/d20charactersheet/aoc2021/day10/SyntaxScoring.kt
+++ Day10_SyntaxScoring/src/main/java/com/d20charactersheet/aoc2021/day10/SyntaxScoring.kt
@@ -1,9 +1,38 @@
 package com.d20charactersheet.aoc2021.day10
 
+import java.util.*
+
 class SyntaxScoring {
 
+    private val openBrackets : List<Char> = listOf('(', '{', '[', '<')
+
     fun checkSyntax(input: String): Int {
+        val programStack = Stack<Char>()
+        for (programPointer in IntRange(0, input.length - 1)) {
+            val statement = input[programPointer]
+            if (openBrackets.contains(statement)) {
+                programStack.push(statement)
+                continue
+            }
+            val popStatement = programStack.pop()
+            if (statement == ')' && popStatement == '(') continue
+            if (statement == '}' && popStatement == '{') continue
+            if (statement == ']' && popStatement == '[') continue
+            if (statement == '>' && popStatement == '<') continue
+            return calculateScore(statement)
+        }
+
         return 0
+    }
+
+    private fun calculateScore(statement: Char): Int {
+        when (statement) {
+            ')' -> return 3
+            ']' -> return 57
+            '}' -> return 1197
+            '>' -> return 25137
+        }
+        throw IllegalArgumentException("unknown statement: $statement")
     }
 
 }
--- Day10_SyntaxScoring/src/test/java/com/d20charactersheet/aoc2021/day10/SyntaxScoringTest.kt
+++ Day10_SyntaxScoring/src/test/java/com/d20charactersheet/aoc2021/day10/SyntaxScoringTest.kt
@@ -6,14 +6,74 @@ import org.junit.jupiter.api.Test
 class SyntaxScoringTest {
 
     @Test
-    fun validateRoundBrackets() {
-        // arrange
-
+    fun checkSyntax_RoundBrackets() {
         // act
         val score = SyntaxScoring().checkSyntax("()")
 
         // assert
         assertThat(score).isEqualTo(0)
+    }
+
+    @Test
+    fun checkSyntax_CurleyBrackets() {
+        // act
+        val score = SyntaxScoring().checkSyntax("{}")
+
+        // assert
+        assertThat(score).isEqualTo(0)
+    }
+
+    @Test
+    fun checkSyntax_SquareBrackets() {
+        // act
+        val score = SyntaxScoring().checkSyntax("[]")
+
+        // assert
+        assertThat(score).isEqualTo(0)
+    }
+
+    @Test
+    fun checkSyntax_AngleBrackets() {
+        // act
+        val score = SyntaxScoring().checkSyntax("<>")
+
+        // assert
+        assertThat(score).isEqualTo(0)
+    }
+
+    @Test
+    fun checkSyntax_CorruptedLine_score57() {
+        // act
+        val score = SyntaxScoring().checkSyntax("(]")
+
+        // assert
+        assertThat(score).isEqualTo(57)
+    }
+
+    @Test
+    fun checkSyntax_CorruptedLine_score25137() {
+        // act
+        val score = SyntaxScoring().checkSyntax("{()()()>")
+
+        // assert
+        assertThat(score).isEqualTo(25137)
+    }
+
+    @Test
+    fun checkSyntax_CorruptedLine_score1197() {
+        // act
+        val score = SyntaxScoring().checkSyntax("(((()))}")
+
+        // assert
+        assertThat(score).isEqualTo(1197)
+    }
+    @Test
+    fun checkSyntax_CorruptedLine_score3() {
+        // act
+        val score = SyntaxScoring().checkSyntax("<([]){()}[{}])")
+
+        // assert
+        assertThat(score).isEqualTo(3)
     }
 
 }
